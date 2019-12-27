14bf6440d3e01ca674e6f0c4481281e0f9b69a9a
bankonly <for21bn01@gmail.com>
2019-12-27 15:09:23 +0700

    Pull

--- databases/migrations/20191227023716-create-user.js
+++ databases/migrations/20191227023716-create-user.js
@@ -14,14 +14,15 @@ module.exports = {
         type: Sequelize.STRING(50)
       },
       phoneNumber: {
-        type: Sequelize.INTEGER(15)
+        type: Sequelize.INTEGER(30)
       },
       reservePhoneNumber: {
-        type: Sequelize.INTEGER(15),
+        type: Sequelize.INTEGER(30),
         allowNull: true
       },
       email: {
-        type: Sequelize.STRING(50)
+        type: Sequelize.STRING(50),
+        allowNull: true
       },
       password: {
         type: Sequelize.STRING(100)
@@ -35,31 +36,37 @@ module.exports = {
         defaultValue: 0
       },
       lastLoginAt: {
-        type: Sequelize.STRING(20),
+        type: Sequelize.DATE,
         defaultValue: Sequelize.NOW,
         allowNull: true
       },
       totalOrder: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        defaultValue: 0
       },
       isBanned: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      isTokenChangeCount: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
       },
       village: {
         type: Sequelize.STRING(50),
-        allowNull: false
+        allowNull: true
       },
       district: {
         type: Sequelize.STRING(50),
-        allowNull: false
+        allowNull: true
       },
       province: {
         type: Sequelize.STRING(50),
-        allowNull: false
+        allowNull: true
       },
       address: {
         type: Sequelize.STRING(50),
-        allowNull: false
+        allowNull: true
       },
       createdAt: {
         defaultValue: Sequelize.NOW,
@@ -77,21 +84,3 @@ module.exports = {
     return queryInterface.dropTable("users");
   }
 };
-
-// const userModel = {
-//   userId: String, // 50 Current date + timeStamp
-//   firstName: String, // 15
-//   lastName: String, // 15
-//   phoneNumber: Number, // 15
-//   reservePhoneNumber: Number, // 15
-//   email: String, // 15
-//   password: String, // 50
-//   isAdmin: Boolean, // Default is 0
-//   onlineTime: Number, // increasing every refresh
-//   lastLoginAt: Date, // current login date
-//   totalOrder: Number, // count order time
-//   isBanned: Boolean, // ban user if user is invalid
-//   village: String, // 50,
-//   district: String, // 50,
-//   province: String, // 50,
-//   address: String // 200
